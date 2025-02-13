fd7c3d9c5cb232e26ed1582bd003967a7b01bed4
Patrick Breen <breenboyz@gmail.com>
2025-02-13 15:01:05 -0600

    :building_construction: Reorganize files into folders

--- ModCompat/CustomRegionCompatability.cs
+++ ModCompat/CustomRegionCompatability.cs
@@ -10,23 +10,12 @@ namespace RainWorldRandomizer
     public static class CustomRegionCompatability
     {
         private static bool hasInit = false;
-        //public static readonly string[] supportedRegions =
-        //{
-        //    "SD", "PA", "FR", "MF", "CW"
-        //};
 
         public static void Init()
         {
             if (hasInit) return;
 
-            Plugin.Log.LogDebug("Init region compat");
             Plugin.OneWayGates.Add("GATE_PA_FR", false);
-
-            //RandomizerMain.LogicBlacklist.AddRange(new List<string>
-            //{
-            //    "Pearl-DSH_Unlore_1", "Pearl-DSH_Unlore_2", "Pearl-DSH_Unlore_3", // Diverse Shelters
-            //    "BigGoldenPearl" // Shrouded Assembly
-            //});
 
             hasInit = true;
         }
