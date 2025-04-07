4673068bf86577c26ddc40110fa956472deaa691
Patrick Breen <breenboyz@gmail.com>
2025-04-06 23:09:53 -0500

    :bookmark: Version 1.2.3 - A hotfix

--- Plugin.cs
+++ Plugin.cs
@@ -18,7 +18,7 @@ namespace RainWorldRandomizer
     {
         public const string PLUGIN_GUID = "salty_syrup.check_randomizer";
         public const string PLUGIN_NAME = "Check Randomizer";
-        public const string PLUGIN_VERSION = "1.2.2";
+        public const string PLUGIN_VERSION = "1.2.3";
 
         internal static ManualLogSource Log;
 
