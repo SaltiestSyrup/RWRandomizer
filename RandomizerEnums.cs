4d08ac9e3f5085e978339e8dc2234d74c21cfb81
Patrick Breen <breenboyz@gmail.com>
2025-04-12 03:11:32 -0500

    :coffin: Remove old gate requirement enum addition

--- RandomizerEnums.cs
+++ RandomizerEnums.cs
@@ -1,4 +1,5 @@
 ﻿using Menu;
+using System;
 
 namespace RainWorldRandomizer
 {
@@ -6,7 +7,6 @@ namespace RainWorldRandomizer
     {
         public static void RegisterAllValues()
         {
-            GateRequirement.RegisterValues();
             SliderId.RegisterValues();
             AbstractObjectType.RegisterValues();
             DataPearlType.RegisterValues();
@@ -14,27 +14,9 @@ namespace RainWorldRandomizer
 
         public static void UnregisterAllValues()
         {
-            GateRequirement.UnregisterValues();
             SliderId.UnregisterValues();
             AbstractObjectType.UnregisterValues();
             DataPearlType.UnregisterValues();
-        }
-
-        // Custom Gate requirement to allow locking all gates and unlocking with checks
-        public class GateRequirement
-        {
-            public static void RegisterValues()
-            {
-                RANDLock = new RegionGate.GateRequirement("L", true);
-            }
-
-            public static void UnregisterValues()
-            {
-                RANDLock?.Unregister();
-                RANDLock = null;
-            }
-
-            public static RegionGate.GateRequirement RANDLock;
         }
 
         public class SliderId
