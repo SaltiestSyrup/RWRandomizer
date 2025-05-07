d301c983403a40d54c65d3e4c103bd29c0dd4906
Patrick Breen <breenboyz@gmail.com>
2025-05-07 12:38:30 -0500

    :bookmark: Version 1.2.7

--- Plugin.cs
+++ Plugin.cs
@@ -18,7 +18,7 @@ namespace RainWorldRandomizer
     {
         public const string PLUGIN_GUID = "salty_syrup.check_randomizer";
         public const string PLUGIN_NAME = "Check Randomizer";
-        public const string PLUGIN_VERSION = "1.2.6";
+        public const string PLUGIN_VERSION = "1.2.7";
 
         internal static ManualLogSource Log;
 
