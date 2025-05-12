c65edfc7b3d6c093693a338a724267b99a6c14e3
Patrick Breen <breenboyz@gmail.com>
2025-05-12 16:55:24 -0500

    :truck: Move TokenCachePatcher out of Generation namespace

--- TokenCachePatcher.cs
+++ TokenCachePatcher.cs
@@ -11,7 +11,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using RWCustom;
 
-namespace RainWorldRandomizer.Generation
+namespace RainWorldRandomizer
 {
     public static class TokenCachePatcher
     {
