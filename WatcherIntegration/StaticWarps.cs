8507cf72b2a99de7d9b1e302545b88573fdce65a
SaltySyrup <sltysyrp@gmail.com>
2026-01-18 20:37:14 -0600

    :sparkles: Add the ability to seal locked warps

--- WatcherIntegration/StaticWarps.cs
+++ WatcherIntegration/StaticWarps.cs
@@ -1,4 +1,7 @@
-﻿using RWCustom;
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RWCustom;
+using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
@@ -21,7 +24,15 @@ namespace RainWorldRandomizer.WatcherIntegration
         /// <summary>
         /// Keeps track of warp tear graphics that should be displaying as locked
         /// </summary>
-        private static ConditionalWeakTable<WarpTear, WarpPoint> ActiveLockedWarps = new();
+        private static ConditionalWeakTable<WarpTear, LockedWarpData> ActiveLockedWarps = new();
+        private class LockedWarpData(WarpPoint self)
+        {
+            public WarpPoint self = self;
+            // Changing the locked status requires access to the SpriteLeaser,
+            // so we store the flag in a bool to be checked in the WarpTear's DrawSprites
+            public bool cancelLockedStatus = false;
+            public bool alreadyResetShader = false;
+        }
 
         /// <summary>
         /// Alt version of <see cref="WarpPoint.LockWarp(bool)"/> that doesn't change any of its data
@@ -72,14 +83,26 @@ namespace RainWorldRandomizer.WatcherIntegration
             {
                 On.Watcher.WarpPoint.Update += WarpPoint_Update;
                 On.Watcher.WarpTear.InitiateSprites += WarpTear_InitiateSprites;
+                On.Watcher.WarpTear.DrawSprites += WarpTear_DrawSprites;
                 On.Watcher.WarpPoint.ExpireWarpByWeaver += WarpPoint_ExpireWarpByWeaver;
+
+                try
+                {
+                    IL.Player.CamoUpdate += Player_CamoUpdate;
+                }
+                catch (Exception e)
+                {
+                    Plugin.Log.LogError(e);
+                }
             }
 
             internal static void RemoveHooks()
             {
                 On.Watcher.WarpPoint.Update -= WarpPoint_Update;
                 On.Watcher.WarpTear.InitiateSprites -= WarpTear_InitiateSprites;
+                On.Watcher.WarpTear.DrawSprites -= WarpTear_DrawSprites;
                 On.Watcher.WarpPoint.ExpireWarpByWeaver -= WarpPoint_ExpireWarpByWeaver;
+                IL.Player.CamoUpdate -= Player_CamoUpdate;
             }
 
             /// <summary>
@@ -94,19 +117,33 @@ namespace RainWorldRandomizer.WatcherIntegration
                     if (self.currentState != WarpPoint.State.Sealed)
                         self.Rando_LockWarp(false);
                     if (self.warpTear is not null && !ActiveLockedWarps.TryGetValue(self.warpTear, out _))
-                        ActiveLockedWarps.Add(self.warpTear, self);
+                        ActiveLockedWarps.Add(self.warpTear, new LockedWarpData(self));
                 }
             }
 
             /// <summary>
             /// Apply custom shader to locked warps to make them red
             /// </summary>
-            private static void WarpTear_InitiateSprites(On.Watcher.WarpTear.orig_InitiateSprites orig, Watcher.WarpTear self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam)
+            private static void WarpTear_InitiateSprites(On.Watcher.WarpTear.orig_InitiateSprites orig, WarpTear self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam)
             {
                 orig(self, sLeaser, rCam);
 
                 if (ActiveLockedWarps.TryGetValue(self, out _))
                     sLeaser.sprites[0].shader = Custom.rainWorld.Shaders["Rando.WarpTear"];
+            }
+
+            private static void WarpTear_DrawSprites(On.Watcher.WarpTear.orig_DrawSprites orig, WarpTear self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
+            {
+                orig(self, sLeaser, rCam, timeStacker, camPos);
+
+                if (ActiveLockedWarps.TryGetValue(self, out LockedWarpData data)
+                    && data.cancelLockedStatus
+                    && !data.alreadyResetShader)
+                {
+                    sLeaser.sprites[0].shader = Custom.rainWorld.Shaders[self.shaderOverride ?? (self.isRippleSide ? "WarpTearRippleSide" : "WarpTear")];
+                    data.alreadyResetShader = true;
+                    Plugin.Log.LogDebug($"Switched shader for now sealed warp");
+                }
             }
 
             /// <summary>
@@ -186,6 +223,65 @@ namespace RainWorldRandomizer.WatcherIntegration
                         }
                     }
                     return null;
+                }
+            }
+
+            /// <summary>
+            /// Seal a locked warp instead of dynamic warping
+            /// </summary>
+            private static void Player_CamoUpdate(ILContext il)
+            {
+                ILCursor c = new(il);
+                ILLabel jumpSkipDynamicWarp = null;
+
+                // Get Player.watcherDynamicWarpInput at 01C5
+                c.GotoNext(x => x.MatchCallOrCallvirt(typeof(Player).GetProperty(nameof(Player.watcherDynamicWarpInput)).GetGetMethod()));
+
+                // Get Player.activateDynamicWarpDuration at 023D
+                c.GotoNext(MoveType.After,
+                    x => x.MatchCallOrCallvirt(typeof(Player).GetProperty(nameof(Player.activateDynamicWarpDuration)).GetGetMethod()),
+                    x => x.MatchBlt(out jumpSkipDynamicWarp)
+                    );
+                c.MoveAfterLabels();
+
+                // Add our own check for sealing nearby warp. If it passes, dynamic warping is skipped
+                c.Emit(OpCodes.Ldarg_0);
+                c.EmitDelegate(SealWarpInsteadOfDynamicWarping);
+                c.Emit(OpCodes.Brtrue, jumpSkipDynamicWarp);
+
+                static bool SealWarpInsteadOfDynamicWarping(Player player)
+                {
+                    Room room = player.room;
+                    MiscWorldSaveData mwsd = room.game.GetStorySession.saveState.miscWorldSaveData;
+
+                    if (player.room.warpPoints.Count == 0) return false;
+
+                    // There should only be one in a room, but iterate anyway just in case
+                    foreach (WarpPoint warp in player.room.warpPoints)
+                    {
+                        // We only care about warps locked by the randomizer
+                        if (!ActiveLockedWarps.TryGetValue(warp.warpTear, out LockedWarpData data)) continue;
+                        // If we just sealed this, don't do it again
+                        if (data.cancelLockedStatus) continue;
+
+                        Plugin.Log.LogDebug($"Sealing warp: {data.self}");
+
+                        string roomName = room.abstractRoom.name.ToLowerInvariant();
+                        string destName = warp.Data.destRoom.ToLowerInvariant();
+                        if (!mwsd.roomsSealedByWeaverAbility.Contains(roomName))
+                            mwsd.roomsSealedByWeaverAbility.Add(roomName);
+                        if (!mwsd.roomsSealedByWeaverAbility.Contains(destName))
+                            mwsd.roomsSealedByWeaverAbility.Add(destName);
+
+                        room.PlaySound(WatcherEnums.WatcherSoundID.Void_Weaver_Ability_Close_Portal);
+                        player.dynamicWarpCooldown = 200;
+                        player.activateDynamicWarpTimer = 0;
+
+                        data.cancelLockedStatus = true;
+                        return true;
+                    }
+
+                    return false;
                 }
             }
         }
