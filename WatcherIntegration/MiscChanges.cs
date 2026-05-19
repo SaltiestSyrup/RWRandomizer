59e82869a6e7eb2c921dfb4972a0ef4806490fae
SaltySyrup <sltysyrp@gmail.com>
2026-05-18 22:42:28 -0500

    :bug: Allow Watcher to do ripple elder quest even if they have dial warp ability

--- WatcherIntegration/MiscChanges.cs
+++ WatcherIntegration/MiscChanges.cs
@@ -1,15 +1,32 @@
-﻿namespace RainWorldRandomizer.WatcherIntegration
+﻿using System;
+using MonoMod.Cil;
+
+namespace RainWorldRandomizer.WatcherIntegration
 {
     public static class MiscChanges
     {
         public static void ApplyHooks()
         {
             On.World.InitiateWeaverPresence += World_InitiateWeaverPresence;
+            
+            try
+            {
+                IL.Player.WatcherUpdate += FixDialWarpAbilityHardcode;
+                IL.Watcher.WatcherRoomSpecificScript.AddRoomSpecificScript += FixDialWarpAbilityHardcode;
+                IL.Watcher.WatcherRoomSpecificScript.WORA_ElderSpawn.Update += FixDialWarpAbilityHardcode;
+            }
+            catch (Exception e)
+            {
+                Plugin.Log.LogError(e);
+            }
         }
-
+        
         public static void RemoveHooks()
         {
             On.World.InitiateWeaverPresence -= World_InitiateWeaverPresence;
+            IL.Player.WatcherUpdate -= FixDialWarpAbilityHardcode;
+            IL.Watcher.WatcherRoomSpecificScript.AddRoomSpecificScript -= FixDialWarpAbilityHardcode;
+            IL.Watcher.WatcherRoomSpecificScript.WORA_ElderSpawn.Update -= FixDialWarpAbilityHardcode;
         }
 
         /// <summary>
@@ -23,6 +40,27 @@
                 return false;
             }
             return orig(self, triggerRoom);
+        }
+        
+        /// <summary>
+        /// Replace the hardcoded "Does the player have dial warp" checks for egg visibility and elder spawning
+        /// </summary>
+        private static void FixDialWarpAbilityHardcode(ILContext il)
+        {
+            ILCursor c = new(il);
+
+            c.GotoNext(MoveType.After,
+                x => x.MatchCallOrCallvirt(typeof(MiscWorldSaveData)
+                    .GetProperty(nameof(MiscWorldSaveData.hasRippleEggWarpAbility)).GetGetMethod()));
+            c.EmitDelegate(HasRippleElderCheck);
+            
+            return;
+
+            static bool HasRippleElderCheck(bool origVal)
+            {
+                // Meet Elder location is collected, or has dial warp if there is no location
+                return Plugin.RandoManager?.IsLocationGiven("Meet_Ripple_Elder") ?? origVal;
+            }
         }
     }
 }
